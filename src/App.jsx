bb4a31f034d80a8d7487ab0c161bec2c95f9b81c
abhishekumar09 <2003abhishek.09@gmail.com>
2024-09-16 13:11:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AOS from "aos";   // FOR SCROLLING
+import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
+
+
+function App() {
+  return (
+    <>
+    <div>App</div>
+    </>
+  )
+}
+
+export default App
