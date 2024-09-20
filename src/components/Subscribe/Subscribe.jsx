fbbdae6d323a725ca921a5dc5cef3c8dcf1630b8
abhishekumar09 <2003abhishek.09@gmail.com>
2024-09-21 00:48:30 +0530

    first commit

---
+++ src/components/Subscribe/Subscribe.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Subscribe = () => {
+  return (
+    <div
+      data-aos="zoom-in"
+      className="mb-20 bg-slate-700 dark:bg-gray-800 text-white "
+    >
+      <div className="container backdrop-blur-sm py-10">
+        <div className="space-y-6 max-w-xl mx-auto">
+          <h1 className="text-2xl !text-center sm:text-left sm:text-4xl font-semibold">
+            Get Notified About New Products
+          </h1>
+          <input
+            data-aos="fade-up"
+            type="text"
+            placeholder="Enter your email"
+            className="w-full p-3"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Subscribe;
